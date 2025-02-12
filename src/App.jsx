e284f64baee4931a665d6c4984b5f1fde0819307
Isaac Yi <isaacyi@Isaacs-MacBook-Air-43.local>
2025-02-12 11:28:16 -0500

    more editing

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,119 @@
-import React from 'react';
-import styles from './App.module.css';
+import React, { useEffect, useRef } from "react";
+import styles from "./App.module.css";
 
 function App() {
+  // const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  // useEffect(() => {
+  //   const handleMouseMove = (e) => {
+  //     setMousePosition({ x: e.clientX, y: e.clientY });
+  //   };
+
+  //   window.addEventListener("mousemove", handleMouseMove);
+
+  //   return () => {
+  //     window.removeEventListener("mousemove", handleMouseMove);
+  //   };
+  // }, []);
+
   return (
     <div className={styles.container}>
+
       {/* left static fixed side */}
       <div className={styles.leftPanel}>
-        <h1>Isaac Yi</h1>
+        <h1>Hi, I'm Isaac</h1>
+        <h3>Software Engineer @ Capital One</h3>
+        <p>Building projects</p>
         <nav>
           <ul>
-            <li><a href="#about">About</a></li>
-            <li><a href="#experience">Experience</a></li>
-            <li><a href="#projects">Projects</a></li>
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#experience">Experience</a>
+            </li>
+            <li>
+              <a href="#projects">Projects</a>
+            </li>
           </ul>
         </nav>
+      </div>
+
+      {/* right scroll side */}
+      <div className={styles.rightPanel}>
+        <section id="about">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </section>
+
+        <section id="experience">
+          {/* <h2>Experience</h2> */}
+          <div className={styles.experienceItem}>
+            <div className={styles.timeSpent}>
+              <p>2023-2025</p>
+            </div>
+            <div className={styles.experienceDetails}>
+              <h3>Software Engineering Intern, Capital One</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident
+              </p>
+            </div>
+          </div>
+
+          {/* more experiences */}
+          <div className={styles.experienceItem}>
+            <div className={styles.timeSpent}>
+              <p>2025-2026</p>
+            </div>
+            <div className={styles.experienceDetails}>
+              <h3>Experience 2 goes here</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident
+              </p>
+            </div>
+          </div>
+        </section>
+
+        <section id="project">
+          <h2>Projects</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident
+          </p>
+        </section>
+      </div>
     </div>
-
-    {/* right scroll side */}
-    <div className={styles.rightPanel}>
-      <section id="about">
-        <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-      </section>
-
-      <section id="experience">
-        <h2>Experiences</h2>
-        <div className={styles.projectCards}>
-          <div className={styles.projectCard}>
-            <h3>experience 1 goes here</h3>
-            <p>worked on things yay</p>
-        </div>
-        {/* more projects */}
-        </div>
-      </section>
-
-      <section id="project">
-        <h2>Projects</h2>
-        <p>email and stuff here</p>
-      </section>
-    </div>
-  </div>
-  )
+  );
 }
 
 export default App;

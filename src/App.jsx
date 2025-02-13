1ac63002f2eda9a011ada54030ed9589c0447088
Isaac Yi <isaacyi@Isaacs-MacBook-Air-43.local>
2025-02-13 14:49:58 -0500

    pre descrution

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
       {/* left static fixed side */}
       <div className={styles.leftPanel}>
         <h1>Hi, I'm Isaac</h1>
-        <h3>Software Engineer @ Capital One</h3>
+        <h3>Software Engineer @ Brown University</h3>
         <p>Building projects</p>
         <nav>
           <ul>
@@ -42,6 +42,7 @@ function App() {
       {/* right scroll side */}
       <div className={styles.rightPanel}>
         <section id="about">
+        <h2>About</h2>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
@@ -63,10 +64,10 @@ function App() {
         </section>
 
         <section id="experience">
-          {/* <h2>Experience</h2> */}
+          <h2>Experience</h2>
           <div className={styles.experienceItem}>
             <div className={styles.timeSpent}>
-              <p>2023-2025</p>
+              <p>06/2024 - 08/2024</p>
             </div>
             <div className={styles.experienceDetails}>
               <h3>Software Engineering Intern, Capital One</h3>
@@ -100,7 +101,7 @@ function App() {
           </div>
         </section>
 
-        <section id="project">
+        <section id="projects">
           <h2>Projects</h2>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
@@ -109,6 +110,21 @@ function App() {
             aliquip ex ea commodo consequat. Duis aute irure dolor in
             reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
             pariatur. Excepteur sint occaecat cupidatat non proident
+
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+            enim ad minim veniam, quis nostrud exercitation ullamco laboris
+            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+            in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+            nulla pariatur. Excepteur sint occaecat cupidatat non proident
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+            enim ad minim veniam, quis nostrud exercitation ullamco laboris
+            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+            in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+            nulla pariatur. Excepteur sint occaecat cupidatat non proident
           </p>
         </section>
       </div>
